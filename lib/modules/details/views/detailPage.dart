c99b5916bd5fdeab36d6b85df221b618e0ff74d7
kumarianushka79701 <kumarianushka79701@gmail.com>
2024-11-06 13:10:26 +0530

    detail page

--- lib/modules/details/views/detailPage.dart
+++ lib/modules/details/views/detailPage.dart
@@ -3,38 +3,63 @@ import 'package:flutter/material.dart';
 import 'package:nonpms_scanner_project/modules/home/views/barcode_page.dart';
 import 'package:nonpms_scanner_project/modules/registration/views/sign_up_page.dart';
 import 'package:nonpms_scanner_project/utils/colors.dart';
+import 'package:nonpms_scanner_project/widgets/button.dart';
 import 'package:nonpms_scanner_project/widgets/common_app_bar.dart';
 import 'package:nonpms_scanner_project/widgets/text_widget%20copy.dart';
 
 class DetailsPage extends StatelessWidget {
   final TextEditingController itemTypeController = TextEditingController();
   final TextEditingController descriptionController = TextEditingController();
+  final TextEditingController consigneeDetailsController =
+      TextEditingController();
+  final TextEditingController consigneeAddressController =
+      TextEditingController();
+  final TextEditingController consigneeMobileController =
+      TextEditingController();
 
   DetailsPage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-    appBar: getAppBar(context, title: getDetailsAppBarTitle(context), onTap: () {
-              Navigator.pop(context);
-
-      }), 
+      appBar:
+          getAppBar(context, title: getDetailsAppBarTitle(context), onTap: () {
+        Navigator.pop(context);
+      }),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Column(
           children: [
             TextField(
               controller: itemTypeController,
-              decoration: const InputDecoration(labelText: 'Item Type (e.g., goods, bike)'),
+              decoration: const InputDecoration(
+                  labelText: 'Item Type (e.g., goods, bike)'),
             ),
             TextField(
               controller: descriptionController,
               decoration: const InputDecoration(labelText: 'Description'),
             ),
+            TextField(
+              controller: consigneeDetailsController,
+              decoration: const InputDecoration(labelText: 'consignee Details'),
+            ),
+            TextField(
+              controller: consigneeAddressController,
+              decoration: const InputDecoration(labelText: 'Consignee Address'),
+            ),
+            TextField(
+              controller: consigneeMobileController,
+              decoration:
+                  const InputDecoration(labelText: 'Consignee Mobile Number'),
+            ),
             const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                if (itemTypeController.text.isNotEmpty && descriptionController.text.isNotEmpty) {
+            AButton(
+              label: "Generate Barcode",
+              textAlign: TextAlign.center,
+              width: MediaQuery.of(context).size.width * 02,
+              onTap: () {
+                 if (itemTypeController.text.isNotEmpty &&
+                    descriptionController.text.isNotEmpty) {
                   Navigator.push(
                     context,
                     MaterialPageRoute(
@@ -49,8 +74,8 @@ class DetailsPage extends StatelessWidget {
                     const SnackBar(content: Text('Please fill all fields')),
                   );
                 }
+              
               },
-              child: const Text('Generate Barcode'),
             ),
           ],
         ),
@@ -58,14 +83,12 @@ class DetailsPage extends StatelessWidget {
     );
   }
 }
+
 Widget getDetailsAppBarTitle(BuildContext context) {
-  
-    return const TextWidget(
-      label: "Details",
-      textColor: AColors.white,
-      fontSize: 18,
-      fontWeight: FontWeight.w700,
-    );
-  }
-
-
+  return const TextWidget(
+    label: "Details",
+    textColor: AColors.white,
+    fontSize: 18,
+    fontWeight: FontWeight.w700,
+  );
+}
